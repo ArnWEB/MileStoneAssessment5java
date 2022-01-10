a223550bfe5a535578211ae538217e88b4603750
ArnWEB <76846944+ArnWEB@users.noreply.github.com>
2022-01-10 11:35:38 +0530

    Delete demo directory

--- demo/src/main/java/com/spring/boot/testing/demo/EmployeeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.spring.boot.testing.demo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmployeeRepository extends JpaRepository<Customer, Long> {
-
-}
